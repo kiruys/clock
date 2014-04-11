eaa06e565351e6896a443eff384dba2d9505fee2
Kirsten Ruys <kirsten.ruys@gmail.com>
2014-04-11 10:33:37 +0200

    refactor of clock variables and added hands

--- drawclock.js
+++ drawclock.js
@@ -2,27 +2,34 @@ $(document).ready(function () {
 	drawClock();
 
 	function drawClock() {
-
-		var canvas, context, centerX, centerY, clock_color, clock_radius, center_color, center_radius;
-		
+		var canvas, context, clock, hands;	
 		canvas = document.getElementById("canvas_clock");
 		context = canvas.getContext('2d');
-		centerX = canvas.width / 2;
-		centerY = canvas.height / 2;
-		clock_radius = canvas.height / 2 * 0.8;
-		clock_color = "yellow";
-		center_color = "black";
-		center_radius = canvas.height * 0.03;
+
+		clock = {	color: 'yellow',
+					centerColor: 'black',
+					radius: canvas.height / 2 * 0.8,
+					centerRadius: canvas.height * 0.01,
+					centerX: canvas.width / 2,
+					centerY: canvas.height / 2
+					};
+		hands = {	hourRadius : canvas.height / 2 * 0.3,
+					minuteRadius: canvas.height / 2 * 0.5,
+					hourColor : 'green',
+					minuteColor : 'red',
+				};
 
 		if (canvas.getContext){
-			drawCircle(clock_radius, clock_color);
-			drawCircle(center_radius, center_color);
+			drawCircle(clock.radius, clock.color);
+			drawCircle(clock.centerRadius, clock.centerColor);
 			drawNumbers();
-
+			drawHand(hands.hourRadius, hands.hourColor, 300);
+			drawHand(hands.minuteRadius, hands.minuteColor, 180);
 		}
+
 		function drawCircle(radius, color) {
 			context.beginPath();
-			context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+			context.arc(clock.centerX, clock.centerY, radius, 0, 2 * Math.PI, false);
 			context.fillStyle = color;
 			context.fill();
 			context.lineWidth = 6;
@@ -31,22 +38,37 @@ $(document).ready(function () {
 		}
 
 		function drawNumbers() {
-			var number_radius, number_angle, numberX, numberY, iString;
-			number_radius = canvas.height / 2 * 0.65;
-			number_angle = 300;
-
+			var number, iString, x, y, coordinates;
+			number = { 	radius: canvas.height / 2 * 0.65,
+						angle: 300
+					};
 			context.font = "bold 50px sans-serif";
 			context.textAlign = "center";
 			context.textBaseline = "middle";
 
 			for (var i = 1; i <= 12; i++) {
-				alert(i);
 				iString = i.toString();
-				numberX = centerX + number_radius * Math.cos(number_angle * (Math.PI /180));
-				numberY = centerY + number_radius * Math.sin(number_angle * (Math.PI/180));
-				context.fillText(iString, numberX, numberY);
-				number_angle += 30;
+				coordinates = getCoordinates(number.radius, number.angle);
+				context.fillText(iString, coordinates[0], coordinates[1]);
+				number.angle += 30;
 			}
+		}
+
+		function drawHand(radius, color, angle) {
+			var coordinates = getCoordinates(radius, angle);
+			
+			context.beginPath();
+			context.moveTo(clock.centerX, clock.centerY);
+			context.lineTo(coordinates[0], coordinates[1]);
+			context.strokeStyle = color;
+			context.stroke();
+		}
+
+		function getCoordinates(radius, angle) {
+			var x, y;
+			x = clock.centerX + radius * Math.cos(angle * (Math.PI /180));
+			y = clock.centerY + radius * Math.sin(angle * (Math.PI/180));
+			return [x, y];
 		}
 	}
 
