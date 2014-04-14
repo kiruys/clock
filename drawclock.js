0ef4cce04093f377db03deccc352e35efde1826a
Kirsten Ruys <kirsten.ruys@gmail.com>
2014-04-14 14:49:58 +0200

    detect click on hand images

--- drawclock.js
+++ drawclock.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 	clock = {	color: 'yellow',
 				centerColor: 'black',
-				radius: canvas.height / 2 * 0.8,
+				radius: canvas.height / 2 * 0.9,
 				centerRadius: canvas.height * 0.01,
 				centerX: canvas.width / 2,
 				centerY: canvas.height / 2
@@ -18,11 +18,23 @@ $(document).ready(function () {
 						color : 'green'
 						},
 				minute : {
-						radius : canvas.height / 2 * 0.25,
+						radius : canvas.height / 2 * 0.30,
 						angle : 270,
 						color : 'red'
+				},
+				lineWidth : 5,
+				image : {
+						url : "images/hand.png",
+						width : 60,
+						height : 90
 				}
 			};
+
+	number =	{ 	radius : canvas.height / 2 * 0.75,
+					angle : 300,
+					font : "bold 30px sans-serif"
+				};
+
 
 	if (canvas.getContext){
 		drawClock();
@@ -31,6 +43,32 @@ $(document).ready(function () {
 
 	$('select#hours').on('change', setClock);
 	$('select#minutes').on('change', setClock);
+	$('#canvas_clock').on('click', moveHand);
+
+	function moveHand(e) {
+		var x;
+		var y;
+		if (e.pageX || e.pageY) { 
+			x = e.pageX;
+			y = e.pageY;
+		}
+		// else { 
+		// 	x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+		// 	y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+		// } 
+		x -= canvas.offsetLeft;
+		y -= canvas.offsetTop;
+		x *= 500/$('#canvas_clock').width();
+		y *= 500/$('#canvas_clock').height();
+
+		if (x > hands.hour.area.left & x < hands.hour.area.right & y > hands.hour.area.top & y < hands.hour.area.bottom) {
+			alert('hourhand');
+		}
+		if (x > hands.minute.area.left & x < hands.minute.area.right & y > hands.minute.area.top & y < hands.minute.area.bottom) {
+			alert('minutehand');
+		}
+
+	}	
 
 	function drawClock() {
 		drawCircle(clock.radius, clock.color);
@@ -42,43 +80,51 @@ $(document).ready(function () {
 		var hour, minutes;
 
 		if (evt === undefined) {
-			drawHand(hands.minute.radius, hands.minute.color, hands.minute.angle);
-			drawHand(hands.hour.radius, hands.hour.color, hands.hour.angle);
+			setHands();
 			return;
 		}
-		if (evt !== undefined) {
-			if ($(evt.target).attr('id') === 'hours') {
-				hour = $(this).val();
-				hands.hour.angle = 270 + (hour * 30);
-			}
-			if ($(evt.target).attr('id') === 'minutes') {
-				minutes = $(this).val();
-				hands.minute.angle = 270 + (minutes * 6);
-			}
+		if (evt !== undefined & $(evt.target).attr('id') === 'hours') {
+			hour = $(this).val();
+			hands.hour.angle = 270 + (hour * 30);
 			drawClock();
-			drawHand(hands.minute.radius, hands.minute.color, hands.minute.angle);
-			drawHand(hands.hour.radius, hands.hour.color, hands.hour.angle);
+			setHands();
+			return;
+		}
+		if (evt !== undefined & $(evt.target).attr('id') === 'minutes') {
+			minutes = $(this).val();
+			hands.minute.angle = 270 + (minutes * 6);
+			drawClock();
+			setHands();
 			return;
 		}
 	}
 
+	function setHands() {
+		drawHand(hands.minute.radius, hands.minute.color, hands.minute.angle);
+		drawHand(hands.hour.radius, hands.hour.color, hands.hour.angle);
+		hands.hour.area = getHandArea(hands.hour.radius, hands.hour.angle);
+		hands.minute.area = getHandArea(hands.minute.radius, hands.minute.angle);
+	}
+
+	function getHandArea(radius, angle) {
+		var coords = getCoordinates(radius + (hands.image.height/2), angle);
+		return {top: coords[1] - 45, bottom: coords[1] + 45, left: coords[0] - 45, right: coords[0] + 45};
+	}
 
 	function drawCircle(radius, color) {
 		context.beginPath();
 		context.arc(clock.centerX, clock.centerY, radius, 0, 2 * Math.PI, false);
 		context.fillStyle = color;
 		context.fill();
-		context.lineWidth = 8;
+		context.lineWidth = 4;
 		context.strokeStyle = '#003300';
 		context.stroke();
 	}
 
 	function drawNumbers() {
-		var number, iString, coordinates;
-		number = { 	radius: canvas.height / 2 * 0.65,
-					angle: 300
-				};
-		context.font = "bold 50px sans-serif";
+		var iString, coordinates;
+
+		context.font = number.font;
 		context.textAlign = "center";
 		context.textBaseline = "middle";
 
@@ -91,12 +137,15 @@ $(document).ready(function () {
 	}
 
 	function drawHand(radius, color, angle) {
-		var coordinates, hand;
+		var coordinates, hand, handEndCoords;
 		coordinates = getCoordinates(radius, angle);
+		
 		hand = new Image();
-		hand.src = "images/hand.png";
+		hand.src = hands.image.url;
 
 		context.beginPath();
+		context.lineWidth = hands.lineWidth;
+		context.lineCap = 'round';
 		context.moveTo(clock.centerX, clock.centerY);
 		context.lineTo(coordinates[0], coordinates[1]);
 		context.strokeStyle = color;
@@ -106,7 +155,7 @@ $(document).ready(function () {
 			context.save();
 			context.translate(coordinates[0], coordinates[1]);
 			context.rotate((angle + 90) * Math.PI/180);
-			context.drawImage(this, -43, -120, 86, 120);
+			context.drawImage(this, hands.image.width/-2, hands.image.height * -1, hands.image.width, hands.image.height);
 			context.restore();
 		}
 
