731d78232b4816c2c35f8462a1f51d38b6fdffb6
Kirsten Ruys <kirsten.ruys@gmail.com>
2014-04-17 12:11:10 +0200

    read clock

--- drawclock.js
+++ drawclock.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 	var canvas, context, clock, hands, startTurn;	
 	canvas = document.getElementById("canvas_clock");
 	context = canvas.getContext('2d');
-	startTurn = false;
 
 	clock = {	color: 'yellow',
 				centerColor: 'black',
@@ -16,26 +15,26 @@ $(document).ready(function () {
 	hands = {	hour : {
 						radius : canvas.height / 2 * 0.15,
 						angle : 180,
-						color : 'green'
+						color : 'green', 
 						},
 				minute : {
 						radius : canvas.height / 2 * 0.30,
 						angle : 270,
-						color : 'red'
+						color : 'red',
 				},
 				lineWidth : 5,
 				image : {
 						url : "images/hand.png",
 						width : 60,
 						height : 90
-				}
+				},
+				on : false
 			};
 
 	number =	{ 	radius : canvas.height / 2 * 0.75,
 					angle : 300,
 					font : "bold 30px sans-serif"
 				};
-
 
 	if (canvas.getContext){
 		drawClock();
@@ -48,26 +47,42 @@ $(document).ready(function () {
 	canvas.addEventListener('mousemove', moveHand);
 	canvas.addEventListener('mouseup', turnOff);
 
-
 	function turnOn(evt) {
-		if (startTurn === false) {
-			startTurn = true;
+		var startCoords;
+		evt.preventDefault();
+		startCoords = getCoords(evt);
+
+		if (hands.on === false) {
+			hands.on = true;
+			hands.active = matchClickCoords(startCoords.x, startCoords.y);
+			return;
 		}
 	}
 
 	function turnOff() {
-		if (startTurn === true) {
-			startTurn = false;
+		if (hands.on === true) {
+			hands.on = false;
+			hands.active = undefined;
+			readClock();
 		}
 	}
 
 	function moveHand(evt) {
-		evt.preventDefault();
-		var x, y, hand;
-
-		if (startTurn === false) {
+		var coords;
+		if (hands.on === false) {
 			return;
 		}
+
+		if (hands.active === 'hour' | hands.active === 'minute') {
+			coords = getCoords(evt);
+			hands[hands.active].angle = getAngle(coords.x, coords.y);
+			drawClock();
+			setHands();
+		}
+	}
+
+	function getCoords(evt) {
+		var x, y;
 
 		if (evt.pageX || evt.pageY) { 
 			x = evt.pageX;
@@ -78,13 +93,8 @@ $(document).ready(function () {
 		y -= canvas.offsetTop;
 		x *= 500/$('#canvas_clock').width();
 		y *= 500/$('#canvas_clock').height();
-
-		hand = matchClickCoords(x, y);
-
-		hands[hand].angle = getAngle(x,y);
-		drawClock();
-		setHands();
-	}
+		return {x: x, y: y};
+	} 
 
 	function matchClickCoords(x, y) {
 		if (x > hands.hour.area.left & x < hands.hour.area.right & y > hands.hour.area.top & y < hands.hour.area.bottom) {
@@ -194,21 +204,56 @@ $(document).ready(function () {
 	}
 
 	function getAngle(xCoord, yCoord) {
-		var p1 = {
+		var p1, p2;
+		p1 = {
 			x: clock.centerX,
 			y: clock.centerY
 		};
- 
-		var p2 = {
+ 		p2 = {
 			x: xCoord,
 			y: yCoord
 		};
- 
-		// angle in radians
-		var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-		 
-		// angle in degrees
+		// angle in radians: var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+		// angle in degrees:
 		return Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
 	}
+
+	function readClock() {
+		var hourAngle, minuteAngle;
+		hourAngle = resetAngle(hands.hour.angle);
+		minuteAngle = resetAngle(hands.minute.angle);
+		$('#clock-time').text(getHour(hourAngle) + ' uur en ' + getMinutes(minuteAngle) + ' minuten');
+	}
+
+	/**
+	 * sets the angle to a number between 0 and 360
+	 */
+	function resetAngle(angle) {
+		if (angle > 360) {
+			return angle -= 360;
+		}
+		if (angle <= 0) {
+			return angle += 360;
+		}
+		if (angle > 0 & angle < 360) {
+			return angle;
+		}
+	}
+
+	function getHour(hourAngle) {
+		var hours = resetAngle(hourAngle-270);
+		return Math.floor(hours/30);
+	}
+
+	function getMinutes(minuteAngle) {
+		var minutes = resetAngle(minuteAngle-270);
+		if (Math.floor(minutes/6) === 60) {
+			return 0;
+		}
+		if (Math.floor(minutes/6) < 60) {
+			return Math.floor(minutes/6);
+		}
+	}
+
 
 });
